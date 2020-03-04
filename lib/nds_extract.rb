0919a402e65ddc5e29c88db7c498fccc09399f0a
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-04 20:47:57 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
+
+# Takes director NDS as argument
+# Returns a hash with director names (strings) as keys, and respective gross totals as values
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -26,6 +29,14 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  director_index = 0
+  director_list = Array.new
+  
+  while director_index < source.size do
+    director_list.push(source[director_index][:name])
+    director_index += 1
+  end
+  director_list
 end
 
 def total_gross(source)
@@ -38,6 +49,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total_gross_of_earnings = 0
+  director_index = 0
+  
+  while director_index < source.size do
+    total_gross_of_earnings += gross_for_director(source[director_index])
+    director_index += 1
+  end
+  total_gross_of_earnings
 end
 
 
